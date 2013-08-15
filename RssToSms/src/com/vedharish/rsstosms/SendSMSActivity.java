d81e323112f7b3b7edb4f348ef411b6753ef0b5f
Harish Ved <ved.harish3@gmail.com>
2013-08-15 17:53:41 +0530

    SMS with intents

--- RssToSms/src/com/vedharish/rsstosms/SendSMSActivity.java
+++ RssToSms/src/com/vedharish/rsstosms/SendSMSActivity.java
@@ -19,6 +19,9 @@ public class SendSMSActivity extends Activity {
     Button buttonSendButton;
     EditText editTextPhoneNumber;
     EditText editTextMessage;
+    
+    String SENT = "SMS_SENT";
+	String DELIVERED = "SMS_DELIVERED";
 	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,8 +47,10 @@ public class SendSMSActivity extends Activity {
 				}
 			}
 		});
+        
+        registerReceiver(new sentBroadcastReceiver(), new IntentFilter(SENT));
+        registerReceiver(new deliveredBroadcastReceiver(), new IntentFilter(DELIVERED));
     }
-
 	public void createToast(String message){
 		Toast.makeText(getBaseContext(), message, Toast.LENGTH_LONG).show();
 	}
@@ -67,11 +72,14 @@ public class SendSMSActivity extends Activity {
 		}
 		@Override
 		protected Void doInBackground(Object... inputObjectArray) {
-			for(int i=0; i<=100; i++){
-				publishProgress(i/100*100);
-			}
+			PendingIntent sentIntent = PendingIntent.getBroadcast(SendSMSActivity.this, 0, new Intent(SENT), 0);
+			publishProgress(1/4*100);
+			PendingIntent deliveredIntent = PendingIntent.getBroadcast(SendSMSActivity.this, 0, new Intent(DELIVERED), 0);
+			publishProgress(2/4*100);
 			SmsManager smsManager = SmsManager.getDefault();
-			smsManager.sendTextMessage((String) inputObjectArray[0], null, (String) inputObjectArray[1], null, null);
+			publishProgress(3/4*100);
+			smsManager.sendTextMessage((String) inputObjectArray[0], null, (String) inputObjectArray[1], sentIntent, deliveredIntent);
+			publishProgress(4/4*100);
 			return null;
 		}
 		@Override
@@ -83,6 +91,41 @@ public class SendSMSActivity extends Activity {
 		protected void onPostExecute(Void result) {
 			super.onPostExecute(result);
 			progressDialog.dismiss();
+		}
+	}
+	public class sentBroadcastReceiver extends BroadcastReceiver {
+		@Override
+		public void onReceive(Context arg0, Intent arg1) {
+			switch(getResultCode()){
+				case Activity.RESULT_OK:
+					SendSMSActivity.this.createToast("SMS sent");
+					break;
+				case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                	SendSMSActivity.this.createToast("Generic failure");
+                    break;
+                case SmsManager.RESULT_ERROR_NO_SERVICE:
+                	SendSMSActivity.this.createToast("No service");
+                    break;
+                case SmsManager.RESULT_ERROR_NULL_PDU:
+                	SendSMSActivity.this.createToast("Null PDU");
+                    break;
+                case SmsManager.RESULT_ERROR_RADIO_OFF:
+                	SendSMSActivity.this.createToast("Radio off");
+                    break;
+			}
+		}
+	}
+	public class deliveredBroadcastReceiver extends BroadcastReceiver {
+		@Override
+		public void onReceive(Context arg0, Intent arg1) {
+			switch (getResultCode()) {
+				case Activity.RESULT_OK:
+		          	SendSMSActivity.this.createToast("SMS delivered");
+		            break;
+		        case Activity.RESULT_CANCELED:
+		          	SendSMSActivity.this.createToast("SMS not delivered");
+		            break;
+		      }
 		}
 	}
 }
